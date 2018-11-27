d6c8ec27aef8b233b147646d643923310db03a8a
sergiopj <sergiopjaranda@gmail.com>
2018-11-27 13:02:18 +0100

    test

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = '@my-seed-test-sergio';
+// google
+module.exports.CLIENT_ID = '581261451125-n77pgqjrclvkti2rthmtohd8uoi03flk.apps.googleusercontent.com';
+module.exports.CLIENT_SECRET = 'EZoA-Xrmy04wYz9pH6TssbwE';
--- routes/login.js
+++ routes/login.js
@@ -11,6 +11,112 @@ const app = express();
 // user model
 const User = require('../models/user');
 
+// google
+const CLIENT_ID = require('../config/config').CLIENT_ID;
+const CLIENT_SECRET = require('../config/config').CLIENT_SECRET;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET);
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        // audience: CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        name: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        googleUser: true
+    }
+}
+
+/* GOOGLE AUTH */
+app.post('/google', async(req, res) => {
+
+    const token = req.body.token;
+
+    console.log('token', token)
+
+    // wait the result of promise 
+    const googleUser = await verify(token)
+        .catch(error => {
+            res.status(403).json({
+                ok: false,
+                message: 'Google Invalid token',
+                error
+            });
+        });
+
+    // check if the google email is already saved in my database
+    User.findOne({ email: googleUser.email }, (err, userDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                message: 'Error loading users',
+                err
+            });
+        }
+
+        // check if this user was authenticated before with google
+        if (userDB) {
+            if (!userDB.google) {
+                return res.status(400).json({
+                    ok: false,
+                    message: 'This user must use normal authentication '
+                });
+            } else {
+                // if the user exists in db and was authenticated before with google
+                // create a token, expires in 8 hours
+                const token = jwt.sign({ user: userDB }, SEED, { expiresIn: 28800 });
+
+                // correct login
+                res.status(200).json({
+                    ok: true,
+                    message: 'Login ok',
+                    token,
+                    user: userDB
+                });
+            }
+        } else {
+            // if the user no exists, create 
+            const user = new User();
+            user.name = googleUser.name;
+            user.email = googleUser.email;
+            user.img = googleUser.img;
+            user.google = true;
+            user.password = 'XXXXX';
+
+            // save user in db
+            user.save((err, userDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        message: 'Error when saving user',
+                        err
+                    });
+                }
+                // create a token, expires in 8 hours
+                const token = jwt.sign({ user: userDB }, SEED, { expiresIn: 28800 });
+                // correct login
+                return res.status(200).json({
+                    ok: true,
+                    message: 'User save in db',
+                    token,
+                    user: userDB
+                });
+            });
+        }
+    });
+});
+
+
+/* MAIN AUTH */
 app.post('/', (req, res) => {
 
     const body = req.body;
@@ -56,16 +162,5 @@ app.post('/', (req, res) => {
     });
 
 });
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
