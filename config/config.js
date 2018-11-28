d0db4503695aadf406e4fd6b0bf657633ba197ac
sergiopj <sergiopjaranda@gmail.com>
2018-11-28 18:33:10 +0100

    test

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports.SEED = '@my-seed-test-sergio';
 // google
-module.exports.CLIENT_ID = '581261451125-n77pgqjrclvkti2rthmtohd8uoi03flk.apps.googleusercontent.com';
-module.exports.CLIENT_SECRET = 'EZoA-Xrmy04wYz9pH6TssbwE';
+module.exports.CLIENT_ID = '581261451125-kctjeddacthavcl156ac57g0gctm0lf9.apps.googleusercontent.com';
+module.exports.CLIENT_SECRET = 'bKsjEM4Gq9U9p-zDqqjpQo1Y';
